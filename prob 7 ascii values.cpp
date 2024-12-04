0d4052f05c10cadf49198a2466027dd8d188678c
ruhanirin999 <yumiella990@gmail.com>
2024-12-04 17:59:30 +0600

    asds

---
+++ prob 7 ascii values.cpp
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+	printf("Ascii value of 'A' is: %d\n" ,'A');
+	printf("Ascii value of '@' is: %d\n" ,'@');
+	return 0;
+}
---
+++ prob3.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	printf("Name:	Mst.Ruhani Ishika.\n");
+	printf("Univ:	Imperial College of Engineering.\n");
+	printf("Dept:	Computer science and engineering.\n");
+	printf("Roll:	23110.\n");
+return 0;		
+}
+
+
+
+
+
