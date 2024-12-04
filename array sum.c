23d0edecaf81292aeefeeff78c8955729ae1da9e
ruhanirin999 <yumiella990@gmail.com>
2024-12-04 17:58:27 +0600

    add

---
+++ array sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int sum=0,i,a[10];
+	scanf("%d",&a[i]);
+	for(i=0;i<10;i++)
+	{
+		sum=sum+a[i];
+	}
+	printf("%d\n",sum);
+return 0;	
+}
---
+++ array.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int i,a[4]={5,6,7,-2};
+    a[2]=9;
+    for(i=0;i<4;i++)
+    {
+        printf("%d\n",a[i]);
+        }
+
+    return 0;
+}
