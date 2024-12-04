3707e9401d36001a741a8d2a69635f3639d084c4
ruhanirin999 <yumiella990@gmail.com>
2024-12-04 17:58:19 +0600

    add

---
+++ ...s
@@ -0,0 +1,6 @@
+#include<stdio.h>//This is the header file that contains some standerd functions and rule sets.
+int main(){
+	//this is the main body where all other code stuffs and logics will be written.
+return 0;//for the reason of return type,we need some integer to return.	
+}
+
