a7091d92c3952b1cb9a35d257089c6c9ea3f8ce7
ruhanirin999 <yumiella990@gmail.com>
2024-12-04 17:59:45 +0600

    add

---
+++ prob 12.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int i=0;
+	int j=0;
+	j=i++ + ++i;
+	printf("%d %d ",i,j);
+	return 0;
+}
---
+++ prob 13.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+	int a,b,x,y;
+	scanf("%d%d%d%d",&a,&b,&x,&y);
+	printf("result is :%d\n",(a*b)+(x*y));
+	return 0;
+	
+}
