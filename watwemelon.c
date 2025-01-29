78da11ab16fc4125315170eeb3521010e061febe
ruhanirin999 <yumiella990@gmail.com>
2025-01-29 19:20:35 +0600

    ok

---
+++ watwemelon.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("Enter the value:");
+    scanf("%d", &n);
+
+        if(n%2==0 && n>2)
+
+          printf("Yes\n");
+        else
+        printf("No\n");
+    
+
+    return 0;
+}
